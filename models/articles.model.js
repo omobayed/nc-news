e4bc401465e0b177bc0980ac7fa93f045a8a28b8
Ola Mobayed <omobayed@Olas-MacBook-Pro.local>
2023-06-27 13:37:04 +0100

    ticket 5 - nc-news-5

--- models/articles.model.js
+++ models/articles.model.js
@@ -10,3 +10,17 @@ exports.selectArticleById = (article_id) => {
             return result.rows[0]
         });
 }
+
+exports.selectAllArticles = () => {
+    const queryStr = 'SELECT articles.author, title, articles.article_id, topic, articles.created_at, article_img_url, articles.votes, COUNT(comment_id) AS comment_count ' +
+        'FROM articles ' +
+        'LEFT OUTER JOIN comments ON articles.article_id = comments.article_id ' +
+        'GROUP BY articles.article_id ' +
+        'ORDER BY created_at DESC;'
+
+    return db
+        .query(queryStr)
+        .then((result) => {
+            return result.rows;
+        })
+}
