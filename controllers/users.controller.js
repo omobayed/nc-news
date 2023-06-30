d51b327592179bb72072e763773981512c60b602
Ola Mobayed <omobayed@Olas-MacBook-Pro.local>
2023-06-30 10:21:09 +0100

    ticket 10- get all users

---
+++ controllers/users.controller.js
@@ -0,0 +1,8 @@
+const{selectAllUsers} = require("../models/users.model")
+exports.getAllUsers= (req,res,next)=>{
+   selectAllUsers()
+   .then((users)=>{
+    res.status(200).send({users})
+   }) 
+   .catch(next)
+}
